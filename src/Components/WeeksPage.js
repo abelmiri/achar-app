a639d0b3dbfcb93e7cd746950b08f358fae1b2c9
Abel Miri <miri.1888@gmail.com>
2020-04-15 03:31:42 +0430

    books added

--- src/Components/WeeksPage.js
+++ src/Components/WeeksPage.js
@@ -12,6 +12,7 @@ class WeeksPage extends PureComponent
             loading: true,
             weeks: [],
         }
+        this.bookWrapper = [React.createRef()]
     }
 
     componentDidMount()
@@ -20,13 +21,28 @@ class WeeksPage extends PureComponent
         axios.get("https://restful.achar.tv/week/", {headers: token ? {"Authorization": `${token}`} : null})
             .then((res) =>
             {
-                this.setState({...this.state, weeks: res.data, loading: false})
+                let weeks = res.data.reduce((sum, num) => ([...sum, {...num, selected: false}]), [])
+                this.setState({...this.state, weeks, loading: false}, () => setTimeout(this.selectDefault, 500))
             })
             .catch((err) =>
             {
-                console.log(" %cERROR ", "color: orange; font-size:12px; font-family: 'Helvetica',consolas,sans-serif; font-weight:900;", err.response)
+                console.log(" %cERROR ", "color: orange; font-size:12px; font-family: 'Helvetica', consolas, sans-serif; font-weight:900;", err.response)
                 this.setState({...this.state, loading: false})
             })
+    }
+
+    selectDefault = () =>
+    {
+        let {weeks} = this.state
+        let modifiedWeeks = weeks.map((w, i) => i === 0 ? {...w, selected: true} : w)
+        this.setState({...this.state, weeks: modifiedWeeks})
+    }
+
+    weekClick = (weekIndex) =>
+    {
+        let {weeks} = this.state
+        let modifiedWeeks = weeks.map((w, i) => i === weekIndex ? {...w, selected: !weeks[weekIndex].selected} : w)
+        this.setState({...this.state, weeks: modifiedWeeks})
     }
 
     render()
@@ -40,9 +56,8 @@ class WeeksPage extends PureComponent
         else return (
             <div className="weeks-wrapper">
                 {
-                    weeks.map((w) =>
-                        <div key={w._id} className="week-element">
-                            {console.log(w)}
+                    weeks.map((w, i) =>
+                        <div key={w._id} className="week-element" onClick={() => this.weekClick(i)} style={{"marginBottom": `${w.selected ? this.bookWrapper[i].scrollHeight + 25 : 25}px`}}>
                             <div>
                                 {w.name}
                             </div>
@@ -52,7 +67,23 @@ class WeeksPage extends PureComponent
                                 {new Date(w.end_date).toLocaleDateString("fa-ir")}
                             </div>
                             <div>
-                                <ArrowSvg className="week-element-arrow-svg"/>
+                                <ArrowSvg className={`week-element-arrow-svg ${w.selected && "selected"}`}/>
+                            </div>
+                            <div className="books-wrapper" ref={e => this.bookWrapper[i] = e}
+                                 style={{"height": `${w.selected ? this.bookWrapper[i].scrollHeight : 0}px`}}>
+                                {
+                                    w.books.map(b =>
+                                        <a key={b._id} className="book-element" href={`https://docs.google.com/viewerng/viewer?url=https://restful.achar.tv${b.file}`}>
+                                            <img alt="book" src={"https://restful.achar.tv" + b.picture} className="book-element-picture"/>
+                                            <div>
+                                                <div className="book-element-name">{b.name}</div>
+                                                <div className="book-element-description">
+                                                    {b.description ? b.description : "طراحان سایت و اپلیکیشن هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند."}
+                                                </div>
+                                            </div>
+                                        </a>,
+                                    )
+                                }
                             </div>
                         </div>)
                 }
