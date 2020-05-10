4f63365d5e720eecd8b2ccdd0ebca399212ff78e
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2020-05-11 00:19:25 +0430

    anime

--- src/Components/Lottery.js
+++ src/Components/Lottery.js
@@ -73,10 +73,10 @@ class Lottery extends PureComponent
                                 <MoonLoader size="70px" color="#707070"/>
                                 :
                                 <div className="lottery-winners" onClick={e => e.stopPropagation()}>
-                                    <div className="winners-section">
+                                    <div className="winners-section anime">
                                         <div className="winners-day">برندگان {split[2]} اردیبهشت</div>
                                         <div className="phone-direction">
-                                            {users.map(user => <div>{user.phone}</div>)}
+                                            {users.map(user => <div key={user.phone}>{user.phone}</div>)}
                                         </div>
                                     </div>
                                 </div>
