75f781a3b137216d629228fe7fab7e139a4fffcc
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2020-05-23 07:56:33 +0430

    winners

--- src/Components/Winners.js
+++ src/Components/Winners.js
@@ -27,6 +27,86 @@ class Winners extends PureComponent
                     </div>
                 </div>
 
+                <div className="winners-section">
+                    <div className="winners-day">برندگان شب سوم، 8 اردیبهشت</div>
+                    <div className="phone-direction">
+                        <div>099225***14</div>
+                        <div>091550***27</div>
+                        <div>091330***85</div>
+                        <div>093723***23</div>
+                    </div>
+                </div>
+
+                <div className="winners-section">
+                    <div className="winners-day">برندگان شب چهارم، 9 اردیبهشت</div>
+                    <div className="phone-direction">
+                        <div>091264***38</div>
+                        <div>093314***17</div>
+                        <div>093919***98</div>
+                        <div>093837***23</div>
+                    </div>
+                </div>
+
+                <div className="winners-section">
+                    <div className="winners-day">برندگان شب پنجم، 10 اردیبهشت</div>
+                    <div className="phone-direction">
+                        <div>091976***58</div>
+                        <div>099102***82</div>
+                        <div>093365***96</div>
+                        <div>091923***91</div>
+                    </div>
+                </div>
+
+                <div className="winners-section">
+                    <div className="winners-day">برندگان شب ششم، 11 اردیبهشت</div>
+                    <div className="phone-direction">
+                        <div>091577***54</div>
+                        <div>093969***11</div>
+                        <div>091243***10</div>
+                        <div>099245***78</div>
+                    </div>
+                </div>
+
+                <div className="winners-section">
+                    <div className="winners-day">برندگان شب هفتم، 12 اردیبهشت</div>
+                    <div className="phone-direction">
+                        <div>091238***15</div>
+                        <div>093829***92</div>
+                        <div>091365***13</div>
+                        <div>093547***31</div>
+                    </div>
+                </div>
+
+                <div className="winners-section">
+                    <div className="winners-day">برندگان شب هشتم، 13 اردیبهشت</div>
+                    <div className="phone-direction">
+                        <div>091801***54</div>
+                        <div>092229***55</div>
+                        <div>099156***08</div>
+                        <div>091099***80</div>
+                    </div>
+                </div>
+
+                <div className="winners-section">
+                    <div className="winners-day">برندگان شب نُهم، 14 اردیبهشت</div>
+                    <div className="phone-direction">
+                        <div>092256***13</div>
+                        <div>093733***20</div>
+                        <div>093348***74</div>
+                        <div>090129***53</div>
+                    </div>
+                </div>
+
+                <div className="winners-section">
+                    <div className="winners-day">برندگان شب دهم، 15 اردیبهشت</div>
+                    <div className="phone-direction">
+                        <div>090510***96</div>
+                        <div>093933***04</div>
+                        <div>091378***02</div>
+                        <div>091201***76</div>
+                    </div>
+                </div>
+
             </div>
         )
     }
